52d4b05ead5a56f2298b66780d93cf0a21d17be4
Paolo Botti <dcube9@users.noreply.github.com>
2023-10-06 11:15:07 +0200

    Project tool-template to Scaffold PostgreSQL Database

---
+++ src/Scaffolder.V70.PostgreSQL/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+/*
+Scaffold-DbContext 
+    'Server=localhost;Port=5432;Database=XYZ;Integrated Security=true;Pooling=true;'
+    Npgsql.EntityFrameworkCore.PostgreSQL
+    -ContextDir DataAccessLayer
+    -OutputDir DataAccessLayer/Entities
+*/
